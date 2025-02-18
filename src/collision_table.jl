5e0152d10f7f9630f8789f659bb14835896199c5
Alejandro Luque Estepa <aluque@iaa.es>
2025-02-18 20:07:35 +0100

    Include a method to construct collision tables.

--- src/collision_table.jl
+++ src/collision_table.jl
@@ -104,3 +104,60 @@ function ratebound(c::ChebyshevCollisionTable{T, N}, eng, pre) where {T, N}
     # k = order
     return sum(ntuple(k -> c.ratebound[k, i + 1] * t[k], Val(N)))    
 end
+
+
+"""
+Build a (Chebyshev) collision table from a list of `processes` for a given particle type `particle`.
+`processes` must be a list of tuples `(coeff, process)` where `coeff` is the multiplicative factor
+that converts cross section per atom to a inverse mean-free-path (typically this is a density of
+scatterers).
+"""
+function collision_table_from_processes(T, processes, particle, Fdt;
+                                        safety=1.1,
+                                        nintervals=32,
+                                        order=3,
+                                        # This upper limit ensures that 1 keV is at an interval
+                                        # boundary and discontinuities
+                                        # there are ok.
+                                        max_energy = 1e3 * co.eV * 2^18)
+    nprocs = length(processes)
+
+    b = BinaryIntervals{Float64}(nintervals, max_energy)
+    a = Array{Float64, 3}(undef, (order, nprocs, nintervals + 1))
+
+    for j in 1:nprocs
+        (coef, proc) = processes[j]
+        a1 = chebfit(eng -> coef * speed(particle, eng) * totalcs(proc, eng), b, order)
+        a[:, j, :] .= reinterpret(reshape, Float64, a1)
+    end
+    
+    rb = compratebound(particle, a, b, Fdt, safety)
+        
+    eng = LogLinRange(log(1e-2 * co.eV), log(0.9999 * max_energy), 100_000)
+    
+    v1 = speed.(Ref(particle), eng)
+    p = sortperm(1:nprocs, by=i ->    
+                 processes[i][1] * maximum(totalcs.(Ref(processes[i][2]), eng) .* v1))
+    
+    proc = tuple([processes[p[i]][2] for i in 1:nprocs]...)
+    a = a[:, p, :]
+    
+    return ChebyshevCollisionTable{Float64, order, typeof(proc), typeof(a),
+                                   typeof(rb)}(;proc=proc, b=b, rate=a, ratebound=rb)    
+end
+
+
+function compratebound(particle, a, b, Fdt, safety)
+    mc2 = co.electron_mass * co.c^2
+    γ(::Type{Electron}, eng) = 1 + eng / mc2
+    γ(::Type{Positron}, eng) = 1 + eng / mc2
+    γ(::Type{Photon}, eng) = 0
+
+    rb = chebfit(b, size(a, 1)) do eng
+        df = sum(j -> Particulator.chebdiff(eng, b, @view a[:, j, :]), axes(a, 2))
+        f = sum(j -> Particulator.chebeval(eng, b, @view a[:, j, :]), axes(a, 2))
+        return f + γ(particle, eng)^3 * speed(particle, eng) * Fdt * abs(df) * safety
+    end
+
+    return rb
+end
