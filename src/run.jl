0eb80ad79653f89a9482fcb9494161da227ddb1d
Alejandro Luque Estepa <aluque@iaa.es>
2025-02-19 16:57:07 +0100

    Fix showing stdev instead of mean.

--- src/run.jl
+++ src/run.jl
@@ -19,7 +19,7 @@ function _msg(mpopl, t, perc)
     nstr = join(map(x -> @sprintf("%15s => %-10s", string(first(x)), repr(last(x))), 
                     map(((a, b),) -> a => nparticles(b), Tuple(pairs(mpopl)))), "\n")
     lstr = join(map(x -> @sprintf("%15s => %-10s", string(first(x)), repr(last(x))), 
-                    map(((a, b),) -> a => spread(b)[2], Tuple(pairs(mpopl)))), "\n")
+                    map(((a, b),) -> a => spread(b)[1], Tuple(pairs(mpopl)))), "\n")
     
     return (@sprintf("%.2f", perc) * "% complete\ntime = " * @sprintf("%.3f", (t / 1e-9)) * " ns\n" * 
         "# of particles:\n" * nstr * 
