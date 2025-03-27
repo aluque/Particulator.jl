cc34cfd8abc4f45a04418fb32113e39d8d5cc603
Alejandro Luque Estepa <luque@dss1.iaa.csic.es>
2025-03-27 12:03:54 +0100

    Implement ConfinedDoubleLayer.

--- src/field.jl
+++ src/field.jl
@@ -15,6 +15,30 @@ end
 
 (f::DoubleLayerField)(x, t) = f.z1 < x[3] < f.z2 ? f.v : zero(f.v)
 
+
+"""
+An electric field with a close form that approximate the electric field of a double layer
+but is confined on x and y.
+"""
+struct ConfinedDoubleLayerField{T}
+    sx::T
+    sy::T
+    sz::T
+    ez0::T
+end
+
+function (f::ConfinedDoubleLayerField)(r, t)
+    (;ez0, sx, sy, sz) = f
+    (x, y, z) = r
+    
+    ex = -(ez0 * exp(-(x^2 / (2*sx^2) + y^2 / (2*sy^2) + z^2 / (2*sz^2))) * x * z) / sx^2
+    ey = -(ez0 * exp(-(x^2 / (2*sx^2) + y^2 / (2*sy^2) + z^2 / (2*sz^2))) * y * z) / sy^2
+    ez = (ez0 * exp(-(x^2 / (2*sx^2) + y^2 / (2*sy^2) + z^2 / (2*sz^2))) 
+          - (ez0 * exp(-(x^2 / (2*sx^2) + y^2/(2*sy^2) + z^2 / (2*sz^2))) * z^2) / sz^2)
+
+    return SA[ex, ey, ez]
+end
+
 struct ElectromagneticField{E, B} <: AbstractForcing
     e::E
     b::B
